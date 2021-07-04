698d80982b6359727b88188341da8517e4dffdf7
bhaskarjha514 <bhaskarjha514@gmail.com>
2021-07-04 18:04:00 +0530

    Task2 completed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, './Client/build')));
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './Client/build/index.html'));
 });
 
